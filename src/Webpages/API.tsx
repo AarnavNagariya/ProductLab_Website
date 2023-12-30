19c6a358ce00a926012256471947c7ae5c39a66e
Aarnav Nagariya <74650049+AarnavNagariya@users.noreply.github.com>
2023-12-31 02:52:38 +0530

    API Complete

--- src/Webpages/API.tsx
+++ src/Webpages/API.tsx
@@ -4,7 +4,7 @@ import Navbar from '../components/Navbar'
 import Header from '../components/Header'
 import About from '../components/About'
 
-import { USR_Generator, TextContent2 } from './API_Content'; // Adjust the path accordingly
+import { USR_Generator, Language_Generator , Sentence_Simplifier , Discourse_Analyzer, Hindi_Parser, USR_Generator2 } from './API_Content'; // Adjust the path accordingly
 
 
 function API() {
@@ -30,12 +30,7 @@ function API() {
             {/* API Box */}
             <div className='API-container'>
               <div className='API-title'>Language Generator</div>
-              <p>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-                Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
-                Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
-                Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-              </p>
+              <Language_Generator />
               <div className='API-buttons'>
                 <button type='button' className='API-doc'>API Doc</button>
                 <button type='button' className='API-get'>Get Access</button>
@@ -44,12 +39,7 @@ function API() {
             {/* API Box */}
             <div className='API-container'>
               <div className='API-title'>Sentence Simplifier</div>
-              <p>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-                Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
-                Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
-                Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-              </p>
+              <Sentence_Simplifier />
               <div className='API-buttons'>
                 <button type='button' className='API-doc'>API Doc</button>
                 <button type='button' className='API-get'>Get Access</button>
@@ -58,12 +48,7 @@ function API() {
             {/* API Box */}
             <div className='API-container'>
               <div className='API-title'>Discourse Analyzer</div>
-              <p>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-                Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
-                Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
-                Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-              </p>
+              <Discourse_Analyzer />
               <div className='API-buttons'>
                 <button type='button' className='API-doc'>API Doc</button>
                 <button type='button' className='API-get'>Get Access</button>
@@ -72,12 +57,7 @@ function API() {
             {/* API Box */}
             <div className='API-container'>
               <div className='API-title'>Hindi Parser</div>
-              <p>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-                Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
-                Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
-                Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-              </p>
+              <Hindi_Parser />
               <div className='API-buttons'>
                 <button type='button' className='API-doc'>API Doc</button>
                 <button type='button' className='API-get'>Get Access</button>
@@ -86,12 +66,7 @@ function API() {
             {/* API Box */}
             <div className='API-container'>
               <div className='API-title'>USR Generator</div>
-              <p>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-                Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
-                Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
-                Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-              </p>
+              <USR_Generator2 />
               <div className='API-buttons'>
                 <button type='button' className='API-doc'>API Doc</button>
                 <button type='button' className='API-get'>Get Access</button>
--- src/Webpages/API_Content.tsx
+++ src/Webpages/API_Content.tsx
@@ -16,11 +16,83 @@ export const USR_Generator = () => {
   );
 }
 
-export const TextContent2 = () => {
-  return (
-    <p>
-      Another set of sentences can go here.<br />
-      Customize the content based on your needs.<br />
-    </p>
-  );
-}
+export const Language_Generator = () => {
+    return (
+        <p className='API-content'>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+        Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+        Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
+        Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+      </p>
+    );
+  }
+
+  export const Sentence_Simplifier = () => {
+    return (
+      <div className='API-content'>
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+      <ul> 
+          <li>Lorem ipsum dolor sit amet, consectetur </li>
+          <li>Lorem ipsum dolor sit amet, consectetur </li>
+          <li>Lorem ipsum dolor sit amet, consectetur </li>
+          <li>Lorem ipsum dolor sit amet, consectetur </li>
+          <li>Lorem ipsum dolor sit amet, consectetur </li>
+          <li>Lorem ipsum dolor sit amet, consectetur </li>
+      </ul>
+      </div>
+    );
+  }
+
+  export const Discourse_Analyzer = () => {
+    return (
+        <p className='API-content'>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+        Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+        Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
+        Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+      </p>
+    );
+  }
+
+  export const Hindi_Parser = () => {
+    return (
+      <div className='API-content'>
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+      <ul> 
+          <li>Lorem ipsum dolor sit amet, consectetur </li>
+          <li>Lorem ipsum dolor sit amet, consectetur </li>
+          <li>Lorem ipsum dolor sit amet, consectetur </li>
+          <li>Lorem ipsum dolor sit amet, consectetur </li>
+          <li>Lorem ipsum dolor sit amet, consectetur </li>
+          <li>Lorem ipsum dolor sit amet, consectetur </li>
+      </ul>
+      </div>
+    );
+  }
+
+  export const USR_Generator2 = () => {
+    return (
+        <p className='API-content'>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+        Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+        Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
+        Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+      </p>
+    );
+  }
+
+//   export const USR_Generator = () => {
+//     return (
+//       <div className='API-content'>
+//       Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+//       <ul> 
+//           <li>Lorem ipsum dolor sit amet, consectetur </li>
+//           <li>Lorem ipsum dolor sit amet, consectetur </li>
+//           <li>Lorem ipsum dolor sit amet, consectetur </li>
+//           <li>Lorem ipsum dolor sit amet, consectetur </li>
+//           <li>Lorem ipsum dolor sit amet, consectetur </li>
+//           <li>Lorem ipsum dolor sit amet, consectetur </li>
+//       </ul>
+//       </div>
+//     );
+//   }
